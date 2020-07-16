73001d549c22a413c0891f1967009991cf2af741
Uday Sharma <sharmauday1999@gmail.com>
2020-07-16 09:13:56 -0700

    added login functionality

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ export default {
       URL: "https://a7dszt7k5l.execute-api.us-east-2.amazonaws.com/prod"
     },
     cognito: {
-      REGION: "us-east-2    ",
-      USER_POOL_ID: " us-east-2_O4SyR4pKq",
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_O4SyR4pKq",
       APP_CLIENT_ID: "5un4nkr6guo4f8mtl7rc3jor3a",
-      IDENTITY_POOL_ID: "us-east-2:785f268a-2d67-45d8-96f0-f414239ae6d5"
+      IDENTITY_POOL_ID: "us-east-2:d19ed9b3-b959-47d6-a070-53e3be870257"
     }
   };
