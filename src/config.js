ec70b5c60d57d6cdb38e87f5637111fd5893a88b
Uday Sharma <sharmauday1999@gmail.com>
2020-07-16 08:28:55 -0700

    add aws integration

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "sheet-music-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://a7dszt7k5l.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2    ",
+      USER_POOL_ID: " us-east-2_O4SyR4pKq",
+      APP_CLIENT_ID: "5un4nkr6guo4f8mtl7rc3jor3a",
+      IDENTITY_POOL_ID: "us-east-2:785f268a-2d67-45d8-96f0-f414239ae6d5"
+    }
+  };
