daf0bf23dca55af839968c26b4e63a0d350736dc
Uday Sharma <sharmauday1999@gmail.com>
2020-07-16 08:23:57 -0700

    added content to react app

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Sheet Music Project</h1>
+        <p>Finding Sheet music has never been easier</p>
+      </div>
+    </div>
+  );
+}
